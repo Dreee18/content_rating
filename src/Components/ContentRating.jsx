f76f54c11a32464588925a70a28867a4a0bd5a29
Andre <andrefellone2004@gmail.com>
2025-09-16 19:23:45 +0800

    Added required contents

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,11 +5,43 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+
+    this.state = {
+      likes: 0,
+      dislikes: 0,
+
+      handleLike:() => {
+        this.setState ((prevState) => ({
+          likes: prevState.likes + 1
+        }))
+      },
+
+      handleDislike:() => {
+        this.setState ((prevState) => ({
+          dislikes: prevState.dislikes + 1
+        }))
+      }
+    };
+
+
   }
   render() {
     return (
      <>
-     <h1>Text Content Rating</h1>
+     <div className="content-rating">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est deleniti tempora quo quasi, nam nesciunt dolorem, placeat pariatur delectus suscipit autem sapiente recusandae reprehenderit quidem iure? Esse neque quaerat tenetur?
+        </p>
+        <div className="rating-buttons">
+          <button className="like-button" onClick={this.state.handleLike}>
+            Like ({this.state.likes})
+          </button>
+
+          <button className="dislike-button" onClick={this.state.handleDislike}>
+            Dislike ({this.state.dislikes})
+          </button>
+        </div>
+     </div>
      </>
     );
   }
